d920b6125ded31b09f386b29782dae3cb25c28d1
Dwi Prastowo <dprastowo@gmail.com>
2020-07-10 04:38:04 +0700

    deploy crud

---
+++ precache-manifest.d81ca42c27cba4e92e8ed48744f085f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ad6ebaceca726d82f249545ce99763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d716e0e6fa61beb2596",
+    "url": "/static/css/main.197fe28b.chunk.css"
+  },
+  {
+    "revision": "46eb2872f2f54edcd325",
+    "url": "/static/js/2.fba1e3ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fba1e3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d716e0e6fa61beb2596",
+    "url": "/static/js/main.2bd546e4.chunk.js"
+  },
+  {
+    "revision": "e8fb2f26e267c67b1068",
+    "url": "/static/js/runtime-main.46b54b2e.js"
+  }
+]);
